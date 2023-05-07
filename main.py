30765dbcdce0db8c5ea988234dbaeb1ba3036726
romkagrek <132187534+romkagrek@users.noreply.github.com>
2023-05-07 10:25:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pygame import *
 clock = time.Clock()
 FPS = 60
 width = 800
-high = 800
+high = 600
 window = display.set_mode((width, high))
 display.set_caption("Пинг - понг")
 window.fill((255,255,255))
@@ -24,16 +24,24 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < 630:
-            self.rect.x += self.speed
-
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < width - 80:
+            self.rect.y += self.speed
+    def update2(self):
+        keys = key.get_pressed()
+        if keys[K_W] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_S] and self.rect.y < width - 80:
+            self.rect.y += self.speed
+a = Player("wall.png", 25, 0, 200, 150, 300)
 game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    a.reset()
+    a.update()
     display.update()
 
     clock.tick(FPS)
